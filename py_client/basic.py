5cc083dae684d88a8f6334496d9a140e95026812
Paweł Kruszko <pawelkruszko@gmail.com>
2024-01-06 18:07:02 +0100

    first api view

---
+++ py_client/basic.py
@@ -0,0 +1,12 @@
+import requests
+
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000"
+
+get_response = requests.get(endpoint, json={"query": "Hello world"})
+print(get_response.text)
+print(get_response.status_code)
+
+# print(get_response.json())
+# print(get_response.status_code)
