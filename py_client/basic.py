9052ed6462b72ab6e5f58c6f11824ffa33fb1320
Paweł Kruszko <pawelkruszko@gmail.com>
2024-01-09 12:50:02 +0100

    model instance update

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,11 +2,11 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/api"
+endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "Hello world"})
-print(get_response.text)
-print(get_response.status_code)
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"})
+# print(get_response.text)
+# print(get_response.status_code)
 
-print(get_response.json()['message'])
+print(get_response.json())
 # print(get_response.status_code)
